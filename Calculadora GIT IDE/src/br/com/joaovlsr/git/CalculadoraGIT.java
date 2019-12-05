161fa69ab4d2a9625fef00080b937701d7819c3f
Joao_ <Joao_@DESKTOP-OQM2KNE>
2019-12-05 18:20:40 -0300

    comecei a programar a calculadora

--- Calculadora GIT IDE/src/br/com/joaovlsr/git/CalculadoraGIT.java
+++ Calculadora GIT IDE/src/br/com/joaovlsr/git/CalculadoraGIT.java
@@ -16,21 +16,6 @@ public class CalculadoraGIT {
         System.out.println("Insira um número: ");
         num1 = in.nextInt();
 
-        System.out.println("Insira outro número: ");
-        num2 = in.nextInt();
-
-        num2 = num1 + num2;
-        System.out.println("O resultado da soma é: "+ num2);
-
-        subtracao = num1 - num2;
-        System.out.println("O resultado da subtração é: "+ subtracao);
-
-        multi = num1 * num2;
-        System.out.println("O resultado da multiplicação é: "+ multi);
-
-        divi = num1 / num2;
-        System.out.println("O resultado da divisão é: " + divi);
-
 
     }
 }
